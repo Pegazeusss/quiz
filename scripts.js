77e639b7d32ff4aec49c403d45b4a0ac4ecce82c
Pegazeusss <petarkwork1@gmail.com>
2023-08-22 10:19:06 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+    question: "How old is Bogo?",
+    a: "10",
+    b: "17",
+    c: "21",
+    d: "26",
+    correct: "c",
+  },
+  {
+    question: "How much longer is Bogo dogo gonna live?",
+    a: "SGIN THE CONTRACT",
+    b: "Year",
+    c: "2 year",
+    d: "Alreay cloned",
+    correct: "a",
+  },
+  {
+    question: "Who won king of the court yesterday?",
+    a: "Me",
+    b: "Sale",
+    c: "Mirza",
+    d: "BOGO",
+    correct: "a",
+  },
+  {
+    question: "Who did best? ",
+    a: "BOGO",
+    b: "BOGO",
+    c: "BOGO MAMA",
+    d: "BOGO",
+    correct: "c",
+  },
+  {
+    question: "HOw much did i get on MIT test? ",
+    a: "A",
+    b: "B",
+    c: "C",
+    d: "FAILED",
+    correct: "b",
+  },
+];
